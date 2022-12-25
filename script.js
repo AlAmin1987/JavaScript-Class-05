cdb75974baa62588177befdec0295d2aa0aa33d6
Md.AlAmin <alamin.dreams2021@gmail.com>
2022-12-25 23:49:55 +0600

    first commit

---
+++ script.js
@@ -0,0 +1,108 @@
+// JavaScript ternary Operator
+// if else example
+
+// let age = 18;
+// if (age >= 18) {
+//   console.log("You are full age person");
+// } else {
+//   console.log("You are not full age person");
+// // }
+// let age = 10;
+// // if (age >= 18) {
+// //   console.log("You are full age person");
+// // } else {
+// //   console.log("You are not full age person");
+// // }
+
+// // ternary Oprator example
+// age >= 18
+//   ? console.log("You are full age person")
+//   : console.log("You are not full age person");
+
+// if else example two
+// let english = 70;
+// // if (english > 80) {
+// //   console.log("You will get A+");
+// // } else {
+// //   console.log("You will leter grade A-");
+// // }
+
+// // Ternary Operator Example
+// english > 80
+//   ? console.log("You will get A+")
+//   : console.log("You will get leter grade A-");
+
+// let age = 20;
+// let food;
+// if (age > 12) {
+//   food = "Chicken Barger";
+// } else food = "Icecream";
+
+// console.log(`He likes to eat ${food}`);
+
+// let age = 18;
+// let food;
+// if (age > 12) {
+//   food = "Chicken Barger";
+// } else food = "Icecream";
+
+// console.log(`He likes to eat ${food}`);
+
+// age > 12
+//   ? console.log(`He likes to eat ${food}`)
+//   : console.log(`He likes to eat ${food}`);
+
+//  example Three
+
+// let age = 3;
+// if (age >= 6) {
+//   console.log("You start your school journey");
+// } else {
+//   console.log("You can't start your school journey");
+// }
+
+// age >= 6
+//   ? console.log("You start your school journey")
+//   : console.log("You can't start your school journey");
+
+// JavaScript Function
+
+// function showName(name) {
+//   console.log(name);
+// }
+// showName("Md.AlAmin");
+// showName("Sompa Jahan");
+
+// function showtext(something) {
+//   console.log(something);
+// }
+// showtext("Write Something!");
+
+// function marks(s1, s2) {
+//   console.log(s1 + s2);
+// }
+// marks(85, 96);
+// marks(85, 80);
+
+// function parameter(s1, s2, s3) {
+//   console.log(s1 + s2 - s3);
+// }
+// parameter(10, 15, 20);
+// parameter(25, 30, 40);
+// parameter(12, 25, 30);
+
+// function averageMark(s1, s2, s3, s4, s5) {
+//   console.log((s1 + s2 + s3 + s4 + s5) / 5);
+// }
+// averageMark(85, 96, 78, 73, 70);
+
+// function averageMark(s1, s2, s3, s4, s5) {
+//   const average = (s1 + s2 + s3 + s4 + s5) / 5;
+//   return average;
+// }
+
+// const asifAverageMark = averageMark(85, 96, 99, 93, 85);
+// console.log(asifAverageMark);
+
+// const nafisAveregeMark = averageMark(89, 92, 77, 93, 85);
+// console.log(nafisAveregeMark);
